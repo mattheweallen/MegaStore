3cccc65107dd41495eacc297e4fd7c64d1260121
Matthew Allen <mattheweallen@gmail.com>
2013-09-28 08:19:53 -0500

    Commit Changes For StoreFront Servlet

---
+++ src/com/entrance/megastore/StoreFront.java
@@ -0,0 +1,43 @@
+package com.entrance.megastore;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StoreFront
+ */
+@WebServlet(description = "This is a Store Front Servlet", urlPatterns = { "/entrance" })
+public class StoreFront extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StoreFront() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+	    out.println("Hello Matthew and Kathy");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
