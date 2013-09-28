d9bac8a54de5756068573bc9a188bcfc82948733
Matthew Allen <mattheweallen@gmail.com>
2013-09-28 15:38:17 -0500

    First draft with Store Title, Drop Down, and Button" git

--- src/com/entrance/megastore/StoreFront.java
+++ src/com/entrance/megastore/StoreFront.java
@@ -2,6 +2,9 @@ package com.entrance.megastore;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,12 +12,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//TODO properly indent code, refer to email from Dr Hunt.
+
 /**
  * Servlet implementation class StoreFront
  */
 @WebServlet(description = "This is a Store Front Servlet", urlPatterns = { "/entrance" })
 public class StoreFront extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Map<String, String> cyclingMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("cx", "Cyclocross");
+        aMap.put("mtb", "Mountain");
+        aMap.put("bmx", "Bicycle Motocross");
+        aMap.put("road", "Road");
+        aMap.put("track", "Track");
+        cyclingMap = Collections.unmodifiableMap(aMap);
+    }
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -30,7 +45,41 @@ public class StoreFront extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
-	    out.println("Hello Matthew and Kathy");
+		
+		try {
+			out.println("<html>"); 
+			out.println("<head>"); 
+			out.println("<title>Cycling</title>");
+            out.println("<style>table{border:1px solid black;}</style>"); 
+            out.println("</head>");  
+            out.println("<body>");
+            out.println("<form>");
+            out.println("<table>");
+            out.println("<tr>");
+            out.println("<th>");
+            out.println("Matt's World of Cycling");
+            out.println("</th>");
+            out.println("<td>");
+            out.println("<select>");
+            for(String s : cyclingMap.keySet()) {
+            	out.println("<option value=\"" + s + "\">" + cyclingMap.get(s) + "</option>");
+            }
+            out.println("</select>");
+            out.println("</td>");
+            out.println("<td>");
+            out.println("<input type=\"submit\" value=\"Submit\"></input>");
+            out.println("</td>");
+            out.println("</th>");
+            out.println("</table>");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>"); 
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			out.close();
+		}
+	    
 	}
 
 	/**
